743cea214a246cb75efdda761791aad66b689609
wong-martin <martin.wong.512@gmail.com>
2016-04-17 13:47:59 -0400

    Changing some inital object placements and some camera settings.
    j

--- Final Project Sample.VS2015/SDL2 Template/Build.cpp
+++ Final Project Sample.VS2015/SDL2 Template/Build.cpp
@@ -56,6 +56,12 @@ namespace utility {
 			else if (keys[SDL_SCANCODE_RIGHT]) {
 				Velocity::Update(seconds);
 			}
+			else if (keys[SDL_SCANCODE_UP]){
+
+			}
+			else if (keys[SDL_SCANCODE_DOWN]) {
+
+			}
 		}
 	};
 
@@ -100,26 +106,30 @@ namespace utility {
 void BuildWorld(game::World& world, gl::Program* program) {
     world.gravity = vec3(0.0f, -2.0f, 0.0f);
     
-    auto octo = new gl::Mesh("octo.obj");
+    auto octo = new gl::Mesh("cube.obj");
     
     gl::Mesh* block = new gl::Mesh("block.obj");
     
     gl::Texture *sand = new gl::Texture(image::TGA("sandstone.tga"));
     gl::Texture *moss = new gl::Texture(image::TGA("lichen.tga"));
-    
-    game::Object* shape = new game::Object(octo, program);
-    shape->Translate(vec3(-1.0f, 2.0f, -4.65f));
-    shape->surface = moss;
-    shape->color = vec4(0.0f, 1.0f, 0.0f, 1.0f);
-    *shape += new physics::Velocity(vec3(0.0f, 0.0f, 0.0f));
-    *shape += new physics::Gravity();
-    *shape += new geometry::SphereCollider(1.0f);
-    world += shape;
-    shape = new game::Object(block, program);
-    shape->Translate(vec3(0.0f, -2.0f, 0.0f));
+	for (int i = 0; i < 10; i++)
+	{
+		game::Object* shape = new game::Object(octo, program);
+		shape->Translate(vec3(-10.0f+(i*2), 2.0f, -10.0f));
+		shape->surface = moss;
+		shape->color = vec4(0.0f, 1.0f, 0.0f, 1.0f);
+		*shape += new physics::Velocity(vec3(0.0f, 0.0f, 0.0f));
+		//*shape += new physics::Gravity();
+		*shape += new geometry::SphereCollider(0.5f);
+		world += shape;
+	}
+	
+	game::Object* shape = new game::Object(octo, program);
+    shape->Translate(vec3(0.0f, -2.0f, -10.0f));
     shape->surface = sand;
-	*shape += new utility::Motor(vec3(1.0f, 0.0f, 0.0f));
-    *shape += new geometry::BoxCollider(6.0f, 0.6f, 3.0f);
+	*shape += new utility::Motor(vec3(0.0f, 10.0f, 0.0f));
+    *shape += new geometry::SphereCollider(0.5f);
     *shape += new physics::Static(0.0f);
     world += shape;
+	
 }
